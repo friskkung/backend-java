6fa90b4810de54ab733d4871c9d53ff410a2f5ce
friskkung <6410210283@psu.ac.th>
2023-09-26 21:38:36 +0700

    Fixed

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.example.demo.model.Student;
 import com.example.demo.repository.StudentRepository;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class StudentController {
 	@Autowired
 	StudentRepository studentRepository;
@@ -50,6 +52,7 @@ public class StudentController {
 
 		try {
 			if (student.isPresent()) {
+				student.get().setStdId(body.getStdId());
 				student.get().setStdName(body.getStdName());
 				student.get().setStdSurName(body.getStdSurName());
 				student.get().setStdEmail(body.getStdEmail());
